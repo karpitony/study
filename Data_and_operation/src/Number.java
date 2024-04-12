87f9777af085fe1ea636428c9f692ca7cd327b6f
Yunseok <watemark1017@gmail.com>
2024-04-12 13:51:24 +0900

    Create Number.java

---
+++ Data_and_operation/src/Number.java
@@ -0,0 +1,12 @@
+public class Number {
+    public static void main(String[] args) {
+
+        System.out.println(6 + 2);  // 8
+        System.out.println(6 - 2);  // 4
+        System.out.println(6 * 2);  // 12
+        System.out.println(6 / 2);  // 3
+
+        System.out.println(Math.floor(Math.PI));    // 3.0
+        System.out.println(Math.ceil(Math.PI));     // 4.0
+    }
+}
