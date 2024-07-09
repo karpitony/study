68f3c6ad53cf36bf4d48e146682308f13766c35f
Yunseok <watemark1017@gmail.com>
2024-07-09 21:47:34 +0900

    learn JS function

--- nomad_js_chromeApp/momentum/app.js
+++ nomad_js_chromeApp/momentum/app.js
@@ -1,13 +1,27 @@
+// function sayHello(nameOfPerson, age) {
+//   console.log("HEllo my name is " + nameOfPerson+ " and I`m " + age);
+// }
+
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+
+function plus(a, b) {
+  console.log(a + b);
+}
+function divide(a, b) {
+  console.log(a / b);
+}
+plus(8, 60);
+divide(98, 20);
+
 const player = {
   name: "nico",
-  points: 10,
-  fat: true,
+  sayHello: function(otherPersonsName){
+    console.log("hello! " + otherPersonsName + " nice to meet you!")
+  }
 };
-console.log(player);
-console.log(player["name"]);
 
-// add new in object
-player.lastName = "potato"
-
-player.points += 15
-console.log(player)
+console.log(player.name);
+player.sayHello("lynn");
