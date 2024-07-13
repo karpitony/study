e0d1960574c11e16eb66d7be671b7e43cc207aae
Yunseok <watemark1017@gmail.com>
2024-07-13 20:38:13 +0900

    js와 html 상호작용

--- 바닐라 JS로 크롬 앱 만들기/momentum/app.js
+++ 바닐라 JS로 크롬 앱 만들기/momentum/app.js
@@ -1,27 +1,4 @@
-// function sayHello(nameOfPerson, age) {
-//   console.log("HEllo my name is " + nameOfPerson+ " and I`m " + age);
-// }
+const title = document.getElementById("title")
 
-// sayHello("nico", 10);
-// sayHello("dal", 23);
-// sayHello("lynn", 21);
+title.innerText = "Got you!"
 
-
-function plus(a, b) {
-  console.log(a + b);
-}
-function divide(a, b) {
-  console.log(a / b);
-}
-plus(8, 60);
-divide(98, 20);
-
-const player = {
-  name: "nico",
-  sayHello: function(otherPersonsName){
-    console.log("hello! " + otherPersonsName + " nice to meet you!")
-  }
-};
-
-console.log(player.name);
-player.sayHello("lynn");
