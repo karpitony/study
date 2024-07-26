7cb856a5fca97f439fb28fd1d6bd581a2200ff16
Yunseok <watemark1017@gmail.com>
2024-07-26 16:18:26 +0900

    Update app.js

--- 바닐라 JS로 크롬 앱 만들기/momentum/app.js
+++ 바닐라 JS로 크롬 앱 만들기/momentum/app.js
@@ -1,7 +1,17 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!")
+  title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick)
+title.addEventListener("mouseenter", handleMouseEnter)
+title.addEventListener("mouseleave", handleMouseLeave)
