a57425d48e87a8fff194d479e8926dad7792a8c8
Yunseok <watemark1017@gmail.com>
2024-07-28 00:28:15 +0900

    Login 구현

--- 바닐라 JS로 크롬 앱 만들기/Productivity App/app.js
+++ 바닐라 JS로 크롬 앱 만들기/Productivity App/app.js
@@ -1,9 +1,30 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting")
 
-function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username)
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function paintGreetings(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}!`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greeting
+  paintGreetings(savedUsername)
+}
