15710e4ee4f411c5e654c1aa96191494464fa80b
Yunseok <watemark1017@gmail.com>
2024-09-01 15:24:18 +0900

    useEffect() 배우기

--- ReactJS로 영화 웹 서비스 만들기/react-movie-service/src/App.js
+++ ReactJS로 영화 웹 서비스 만들기/react-movie-service/src/App.js
@@ -1,11 +1,29 @@
-import Button from './Button';
-import Styled from './App.module.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
   return (
     <div>
-      <h1 className={Styled.title}>Welcome back!!!</h1>
-      <Button text={"Continue"}/>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
