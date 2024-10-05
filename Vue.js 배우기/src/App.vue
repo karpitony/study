bcf6e4b857bc99c3dcd0cbdbf4de3b6517fe6f91
Yunseok <watemark1017@gmail.com>
2024-10-06 01:13:39 +0900

    Vue.js 생명주기 배우기

---
+++ Vue.js 배우기/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+  <h1>Vue.js Lifecycle Test</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count : 0
+    }
+  },
+  beforeCreate() {
+    console.log('Lifecycle: beforeCreate', this.count)
+  // this.test()
+  },
+  created() {
+    console.log('Lifecycle: created', this.count)
+    this.test()
+  },
+  // beforeMount() {
+  //   console.log('Lifecycle: beforeMount', document.querySelector('h1'))
+  // },
+  // mounted() {
+  //   console.log('Lifecycle: mounted', document.querySelector('h1'))
+  // },
+  methods: {
+    test() {
+      console.log("함수호출!!!")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
