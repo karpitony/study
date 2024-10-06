d4571b82e29b2f008d0f209a6cd137e62ffc32ac
Yunseok <watemark1017@gmail.com>
2024-10-06 13:12:14 +0900

    선언적 렌더링 & 클래스, 스타일 바인딩  실습

--- Vue.js 배우기/src/App.vue
+++ Vue.js 배우기/src/App.vue
@@ -1,39 +1,40 @@
 <template>
-  <div>
-    {{ count }}
-  </div>
-  <h1>Vue.js Lifecycle Test</h1>
+  <div>{{ rawHTML }}</div>  
+  <div>{{ rawHTML2 }}</div>
+  <h1 v-html="rawHTML2"></h1>
+
+  <h2 v-bind:class="{ active: isActive }">클래스 바인딩 테스트입니다.</h2>
+  <h2 :class="{ active: isActive }">클래스 바인딩 테스트입니다.</h2>
+  <button @click="change">버튼</button>
+
+  <h3 style="color: red; font-size: 24px">스타일 바인딩 테스트입니다.</h3>
+  <h3 :style="{ color: fontColor, fontSize: fontSize}">스타일 바인딩 테스트입니다.</h3>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count : 0
+      rawHTML: "이것은 텍스트입니다.",
+      rawHTML2: "<span style='color: red'>이것은 빨간색이여야 합니다.</span>",
+      isActive: false,
+      fontColor: '#888888',
+      fontSize: '48px'
     }
   },
-  beforeCreate() {
-    console.log('Lifecycle: beforeCreate', this.count)
-  // this.test()
-  },
-  created() {
-    console.log('Lifecycle: created', this.count)
-    this.test()
-  },
-  // beforeMount() {
-  //   console.log('Lifecycle: beforeMount', document.querySelector('h1'))
-  // },
-  // mounted() {
-  //   console.log('Lifecycle: mounted', document.querySelector('h1'))
-  // },
   methods: {
-    test() {
-      console.log("함수호출!!!")
+    change() {
+      this.isActive = !this.isActive;
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+
+h2.active {
+  color: green  ;
+}
 
 </style>
