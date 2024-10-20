c96a6bc6bc6b85bd648e4703a72871dc164e8c52
Yunseok <watemark1017@gmail.com>
2024-10-20 16:59:30 +0900

    서버사이드 fetch, loading 컴포넌트, 병렬 요청

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,28 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Home',
+};
+
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log('I`m fetching movies');
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
 }
 
-export default function Page() {
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      Hello!
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,34 @@
+import { API_URL } from '../../../(home)/page';
+
 interface MovieDetailProps {
   params: {
     id: string;
   };
 }
 
-export default function MovieDetail({params: {id}}: MovieDetailProps) {
-  console.log(id);
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(API_URL + `/${id}`);
+  return await response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(API_URL + `/${id}/videos`);
+  return await response.json();
+}
+
+export default async function MovieDetail({params: {id}}: MovieDetailProps) {
+  console.log('======================');
+  console.log('start fetching');
+  // const movie = await getMovie(id);
+  // const videos = await getVideos(id);
+  const [movie, videos ] = await Promise.all([getMovie(id), getVideos(id)])
+
+  console.log('end fetching');
   return (
-    <h1>Movie {id}</h1>
+    <h1>{movie.title}</h1>
   );
 }
