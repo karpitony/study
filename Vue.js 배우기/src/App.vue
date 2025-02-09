a78eb74a9dff64dcad7df76c2f12fb914eee495a
Yunseok <watemark1017@gmail.com>
2025-02-09 15:04:32 +0900

    Update App.vue

--- Vue.js 배우기/src/App.vue
+++ Vue.js 배우기/src/App.vue
@@ -1,40 +1,38 @@
 <template>
-  <div>{{ rawHTML }}</div>  
-  <div>{{ rawHTML2 }}</div>
-  <h1 v-html="rawHTML2"></h1>
-
-  <h2 v-bind:class="{ active: isActive }">클래스 바인딩 테스트입니다.</h2>
-  <h2 :class="{ active: isActive }">클래스 바인딩 테스트입니다.</h2>
-  <button @click="change">버튼</button>
-
-  <h3 style="color: red; font-size: 24px">스타일 바인딩 테스트입니다.</h3>
-  <h3 :style="{ color: fontColor, fontSize: fontSize}">스타일 바인딩 테스트입니다.</h3>
-
+  <div>
+    <div v-if="isVisible" class="red"></div>
+    <div v-if="isVisible === true" class="blue"></div>
+    
+    <div v-else class="black"></div>
+    <button @click="isVisible = !isVisible">Toggle</button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      rawHTML: "이것은 텍스트입니다.",
-      rawHTML2: "<span style='color: red'>이것은 빨간색이여야 합니다.</span>",
-      isActive: false,
-      fontColor: '#888888',
-      fontSize: '48px'
-    }
-  },
-  methods: {
-    change() {
-      this.isActive = !this.isActive;
+      isVisible: true,
+
     }
   }
 }
 </script>
 
 <style scoped>
-
-h2.active {
-  color: green  ;
+.red {
+  width: 100px;
+  height: 100px;
+  background-color: red;
 }
-
+.blue {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
+.black {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+}
 </style>
